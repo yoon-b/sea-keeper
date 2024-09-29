73ae6a0192575fc0b528ad1fb0ddf63cd9fb13f0
yoon-b <eunbi.kim.kor@gmail.com>
2024-09-29 22:42:26 +0900

    ReFactor: 변수 타입 정의

--- src/api/reportApi.ts
+++ src/api/reportApi.ts
@@ -86,7 +86,7 @@ export const fetchCleanupReport = async (page: number) => {
 export const fetchCleanupReportById = async (reportId: number) => {
   try {
     const res = await axiosInstance.get(`/cleanup/${reportId}`);
-    console.log(res.data.result);
+    // console.log(res.data.result);
     return res.data.result;
   } catch (err) {
     console.log("failed to fetch data", err);
--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,17 @@
 interface User {
   name: string;
 }
+
+interface Cleanup {
+  id: number;
+  serialNumber: string;
+  latitude: number;
+  longitude: number;
+  coastName: string;
+  coastLength: number;
+  actualTrashVolume: number;
+  mainTrashType: number;
+  beforeViewImageUrl: string;
+  afterViewImageUrl: string;
+  completeViewImageUrl: string;
+}
