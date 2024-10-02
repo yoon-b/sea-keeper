ab9f571c3c3e54510322b07fdebc326c53651bf1
yoon-b <eunbi.kim.kor@gmail.com>
2024-10-02 11:50:41 +0900

    Design: 지도 타일레이어 제공자 변경

--- src/pages/Collector/Collector.tsx
+++ src/pages/Collector/Collector.tsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import { useCurrentLocation } from "../../utils/useCurrentLocation";
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents, useMap } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+  useMap,
+} from "react-leaflet";
 import { Icon, LatLngTuple } from "leaflet";
 import NoPickupTrashSpots from "../../components/Collector/NoPickupTrashSpots";
 import UserLocationCircle from "../../components/Collector/UseLocationCircle";
@@ -11,7 +18,7 @@ import { MdMyLocation } from "react-icons/md";
 import RouteSummary from "../../components/Collector/RouteSummary";
 
 const userIcon = new Icon({
-  iconUrl: '/animated-marker.svg',
+  iconUrl: "/animated-marker.svg",
   iconSize: [50, 50], // SVG 아이콘 크기
   iconAnchor: [25, 25], // 아이콘의 중심점
   popupAnchor: [0, -15], // 팝업의 위치 조정
@@ -35,16 +42,20 @@ const Collector = () => {
   const currentLocation = useCurrentLocation();
   const [zoomLevel, setZoomLevel] = useState<number>(10);
   const [isCollapsed, setIsCollapsed] = useState(false);
-  const [selectedMarkers, setSelectedMarkers] = useState<Set<TrashData>>(new Set());
+  const [selectedMarkers, setSelectedMarkers] = useState<Set<TrashData>>(
+    new Set()
+  );
   const [trashSum, setTrashSum] = useState<number>(0);
-  const [routeSections, setRouteSections] = useState<RouteSection[] | null>(null);
+  const [routeSections, setRouteSections] = useState<RouteSection[] | null>(
+    null
+  );
   const [routeSummary, setRouteSummary] = useState<RouteSummary | null>(null);
 
   const HandleZoomChange = () => {
     const map = useMapEvents({
       zoomend: () => {
         setZoomLevel(map.getZoom());
-      }
+      },
     });
     return null;
   };
@@ -56,15 +67,19 @@ const Collector = () => {
   return (
     <div className="flex justify-center items-center w-screen h-screen">
       <MapContainer
-        center={currentLocation || [35.1689, 129.1360]}
+        center={currentLocation || [35.1689, 129.136]}
         zoom={zoomLevel}
         scrollWheelZoom={true}
         className="w-full h-full z-10"
       >
-        <TileLayer
+        {/* <TileLayer
             url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://stadiamaps.com">Stadia Maps</a>'
-          />
+          /> */}
+        <TileLayer
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://carto.com/">Carto</a>'
+        />
         {currentLocation && (
           <Marker position={currentLocation} icon={userIcon}>
             <Popup>현재 위치</Popup>
@@ -79,17 +94,19 @@ const Collector = () => {
           zoomLevel={zoomLevel}
         />
         <RoutePolyline
-        zoomLevel={zoomLevel}
-        routeSections={routeSections}
-        routeSummary={routeSummary}
+          zoomLevel={zoomLevel}
+          routeSections={routeSections}
+          routeSummary={routeSummary}
         />
-        <MyLocationButton currentLocation={currentLocation || [35.1689, 129.1360]} />
+        <MyLocationButton
+          currentLocation={currentLocation || [35.1689, 129.136]}
+        />
       </MapContainer>
-      
-       {/* BottomSheet */}
-       <div
+
+      {/* BottomSheet */}
+      <div
         className={`fixed bottom-0 bg-white rounded-t-lg shadow-lg transition-transform duration-300 z-10 w-full ${
-          isCollapsed ? 'transform translate-y-[20%]' : 'h-[50vh]'
+          isCollapsed ? "transform translate-y-[20%]" : "h-[50vh]"
         }`}
       >
         {/* BottomSheet 헤더 */}
@@ -97,11 +114,11 @@ const Collector = () => {
           className="flex items-center justify-center bg-blue-300 text-white h-[5vh] cursor-pointer rounded-t-lg z-10"
           onClick={toggleBottomSheet}
         >
-          {isCollapsed ? '▲' : '▼'}
+          {isCollapsed ? "▲" : "▼"}
         </div>
 
         {/* BottomSheet 내용 */}
-        {!isCollapsed &&
+        {!isCollapsed && (
           <div className="p-4 h-[90%] overflow-y-scroll text-black">
             <ClosestTrashSpot currentLocation={currentLocation} />
             {routeSummary ? (
@@ -112,30 +129,36 @@ const Collector = () => {
                 setRouteSections={setRouteSections}
                 setRouteSummary={setRouteSummary}
               />
-            ):(
-            <SelectedTrashSpots
-              selectedMarkers={selectedMarkers}
-              setSelectedMarkers={setSelectedMarkers}
-              setRouteSections={setRouteSections}
-              setRouteSummary={setRouteSummary}
-              currentLocation={currentLocation}
-              trashSum={trashSum}
+            ) : (
+              <SelectedTrashSpots
+                selectedMarkers={selectedMarkers}
+                setSelectedMarkers={setSelectedMarkers}
+                setRouteSections={setRouteSections}
+                setRouteSummary={setRouteSummary}
+                currentLocation={currentLocation}
+                trashSum={trashSum}
               />
             )}
-            <div className="flex flex-start mt-4 text-sm text-gray-600">※ 수거 완료하였다면 쓰레기 아이콘을 길게 눌러서 수거해주세요</div>
+            <div className="flex flex-start mt-4 text-sm text-gray-600">
+              ※ 수거 완료하였다면 쓰레기 아이콘을 길게 눌러서 수거해주세요
+            </div>
           </div>
-        }
+        )}
       </div>
     </div>
   );
 };
 
-const MyLocationButton = ({ currentLocation } : {currentLocation: LatLngTuple }) => {
+const MyLocationButton = ({
+  currentLocation,
+}: {
+  currentLocation: LatLngTuple;
+}) => {
   const map = useMap();
 
   const handleMyLocationClick = () => {
     if (currentLocation) {
-      map.setView(currentLocation, map.getZoom());  // 현재 위치로 중심 이동
+      map.setView(currentLocation, map.getZoom()); // 현재 위치로 중심 이동
     }
   };
 
