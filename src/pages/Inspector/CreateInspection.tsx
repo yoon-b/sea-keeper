5b5e050742d6a7f0af4ca5da87866a24d98a04bb
yoon-b <eunbi.kim.kor@gmail.com>
2024-10-04 00:19:53 +0900

    Design: 조사 기록 작성 페이지 수정

--- src/pages/Inspector/CreateInspection.tsx
+++ src/pages/Inspector/CreateInspection.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { createInspectionReport } from "../../api/reportApi";
@@ -42,7 +42,6 @@ const CreateInspection = () => {
     try {
       const res = await createInspectionReport(formData);
       navigate(`/report-detail/${res.result}`);
-      // console.log(res);
     } catch (err) {
       console.log("조사 기록 작성 실패", err);
     }
@@ -69,87 +68,104 @@ const CreateInspection = () => {
   };
 
   return (
-    <React.Fragment>
-      <form onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data">
-        <div className="flex items-center justify-between">
-          <label className="py-2">해안명</label>
-          <input
-            {...register("coastName")}
-            className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4"
-            required
-            type="text"
-          />
-        </div>
+    <div className="text-black m-4">
+      <h3 className="text-lg font-semibold text-slate-800 mb-2 p-2">
+        조사 기록 작성하기
+      </h3>
 
-        <div className="flex items-center justify-between">
-          <label className="py-2">해안 길이</label>
+      <form onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data">
+        <input
+          {...register("coastName")}
+          className="report-input-field"
+          required
+          type="text"
+          placeholder="해안명*"
+        />
+
+        <div className="relative w-full h-[50%] my-2">
           <input
             {...register("coastLength")}
-            className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4"
+            className="report-input-field"
             required
             type="number"
+            placeholder="해안 길이*"
           />
-          m
+          <p className="report-input-unit">m</p>
         </div>
 
-        <div className="flex items-center justify-between">
-          <label className="py-2">수거 예측량</label>
+        <div className="relative w-full h-[50%] my-2">
           <input
             {...register("estimatedAmount")}
-            className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4"
+            className="report-input-field"
             required
             type="number"
+            placeholder="수거 예측량*"
           />
-          개
+          <p className="report-input-unit">개</p>
         </div>
 
-        <label className="font-semibold py-2">주요 쓰레기 종류</label>
-        <select
-          {...register("mainWasteType")}
-          className="block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4"
-          required
-        >
-          <option value="1">폐어구류 (그물, 밧줄, 양식 자재 등)</option>
-          <option value="2">부표류 (스티로폼 부표, 인증부표 등)</option>
-          <option value="3">
-            생활쓰레기류 (음료수병, 포장비닐, 과자봉지, 캔 등)
-          </option>
-          <option value="4">대형 투기쓰레기류 (가전제품, 타이어 등)</option>
-          <option value="5">초목류 (자연목, 인공목 등)</option>
-        </select>
+        <div className="flex flex-col my-2">
+          <label className="text-gray-500 py-2 text-left">주요 쓰레기:</label>
+          <select
+            {...register("mainWasteType")}
+            className="report-input-field"
+            required
+            defaultValue=""
+          >
+            <option value="" disabled>
+              주요 쓰레기 종류를 선택하세요.
+            </option>
+            <option value="1">폐어구류 (그물, 밧줄, 양식 자재 등)</option>
+            <option value="2">부표류 (스티로폼 부표, 인증부표 등)</option>
+            <option value="3">
+              생활쓰레기류 (음료수병, 포장비닐, 과자봉지, 캔 등)
+            </option>
+            <option value="4">대형 투기쓰레기류 (가전제품, 타이어 등)</option>
+            <option value="5">초목류 (자연목, 인공목 등)</option>
+          </select>
+        </div>
 
-        <div>
-          <label htmlFor="photo" className="py-2">
+        <div className="flex flex-col my-2">
+          <label htmlFor="photo" className="text-gray-500 py-2 text-left">
             해안사진:
           </label>
           <input
             type="file"
             id="photo"
             accept="image/*"
-            // capture="environment"
             {...register("photo")}
           />
         </div>
 
-        <div className="py-2">
-          <button type="button" onClick={getCurrentLocation}>
-            현재 위치 입력
-          </button>
+        <div className="my-2">
+          <p className="text-gray-500 py-2 text-left">현재 위치:</p>
+          {location ? (
+            <div className="flex justify-around items-center">
+              <span>위도: {location.latitude.toFixed(4)}</span>
+              <span>경도: {location.longitude.toFixed(4)}</span>
+            </div>
+          ) : (
+            <button
+              type="button"
+              onClick={getCurrentLocation}
+              className="text-sm shadow-sm font-medium tracking-wider border border-gray-500 rounded-md bg-transparent text-black"
+            >
+              현재 위치 입력하기
+            </button>
+          )}
         </div>
-
-        {location && (
-          <div>
-            <p>위도: {location.latitude}</p>
-            <p>경도: {location.longitude}</p>
-          </div>
-        )}
 
         <input type="hidden" {...register("latitude")} />
         <input type="hidden" {...register("longitude")} />
 
-        <button type="submit">작성하기</button>
+        <button
+          type="submit"
+          className="w-full text-base shadow-sm font-medium tracking-wider text-white rounded-md mt-8"
+        >
+          작성하기
+        </button>
       </form>
-    </React.Fragment>
+    </div>
   );
 };
 
