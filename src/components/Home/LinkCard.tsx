a307e371bca0cf5abb417d0bb649993a4d0087b1
yoon-b <eunbi.kim.kor@gmail.com>
2024-10-05 11:56:41 +0900

    Feat: 사용자 권한별 기능 분리

--- src/components/Home/LinkCard.tsx
+++ src/components/Home/LinkCard.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
 import CleaningServicesOutlinedIcon from "@mui/icons-material/CleaningServicesOutlined";
 import LocalShippingOutlinedIcon from "@mui/icons-material/LocalShippingOutlined";
+import InsertChartOutlinedIcon from "@mui/icons-material/InsertChartOutlined";
 
 interface LinkCardProps {
   to: string;
@@ -22,6 +23,9 @@ const LinkCard: React.FC<LinkCardProps> = ({ to, title, icon }) => {
     case "truck":
       IconComponent = LocalShippingOutlinedIcon;
       break;
+    case "chart":
+      IconComponent = InsertChartOutlinedIcon;
+      break;
     default:
       IconComponent = AddAPhotoIcon;
   }
@@ -29,12 +33,10 @@ const LinkCard: React.FC<LinkCardProps> = ({ to, title, icon }) => {
   return (
     <div
       className={`relative group h-48 flex flex-col items-center justify-center rounded-xl shadow-md bg-blue-300 bg-opacity-60`}
+      style={{ backgroundColor: "#1d2268" }}
     >
-      <Link to={to} className="text-black">
+      <Link to={to} className="text-white">
         <div className="h-20 pt-4">
-          {/* <div
-            className={`absolute top-3 lg:top-[-10%] left-[5%] z-2 group-hover:top-[-40%] group-hover:opacity-[0.9] duration-300 w-[90%] h-28 bg-blue-300 rounded-xl justify-items-center align-middle`}
-          /> */}
           <IconComponent
             className="text-white mx-auto"
             style={{ width: "60px", height: "60px" }}
