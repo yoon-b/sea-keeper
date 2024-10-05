8e104f8577ff26020d065876e0196fd65c2b02d9
yoon-b <eunbi.kim.kor@gmail.com>
2024-10-05 20:55:28 +0900

    Feat: 큰글자모드 토글

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,9 +25,30 @@ const Home = () => {
     };
   }, []);
 
+  const handleToggle = () => {
+    setIsManager((prev) => !prev);
+  };
+
   return (
-    <div className="page-container flex flex-col">
-      <LinkCardGrid mode={isManager ? "manager" : "default"} />
+    <div>
+      <div className="flex justify-center items-center w-full fixed top-28">
+        {/* <div className="flex justify-center items-center fixed right-4 bottom-4"> */}
+        <p className="text-indigo-900 mr-1 font-semibold">큰글자 모드</p>
+        <label className="relative inline-flex cursor-pointer items-center">
+          <input
+            id="switch"
+            type="checkbox"
+            className="peer sr-only"
+            checked={!isManager}
+            onChange={handleToggle}
+          />
+          <label htmlFor="switch" className="hidden"></label>
+          <div className="peer h-6 w-11 rounded-full border bg-slate-200 after:absolute after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-indigo-900 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-green-300"></div>
+        </label>
+      </div>
+      <div className="page-container flex flex-col">
+        <LinkCardGrid mode={isManager ? "manager" : "default"} />
+      </div>
     </div>
   );
 };
